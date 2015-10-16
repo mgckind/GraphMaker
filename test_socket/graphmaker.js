bd198c4634197e25d6c8ffc07ab82461253f1a7a
Matias Carrasco Kind <mgckind@gmail.com>
2015-10-15 23:26:28 -0500

    keep graph status across spaces

--- test_socket/graphmaker.js
+++ test_socket/graphmaker.js
@@ -10,8 +10,10 @@ var io = require('socket.io')(http);
 var joint = require('jointjs');
 var graph = new joint.dia.Graph;
 
-var newpages = [];
+var newpages = ['default'];
+var graphdict = {};
 
+graphdict['default']=graph;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res){
@@ -19,49 +21,59 @@ app.get('/', function (req, res){
 });
 
 io.on('connection', function (socket){
-    socket.emit('load0',graph.toJSON());
 
+    socket.on('shakehand', function(path){;
+    path = path.slice(1);
+    console.log(path);
+    if (path == '') {
+        path = 'default'
+    }
+    socket.room = path;
+    socket.join(socket.room);
+    socket.graph = graphdict[path];
+    socket.emit('load0',socket.graph.toJSON());
+    console.log('a user connected to room '+socket.room);
+    });
 
-    console.log('a user connected');
     
     socket.on('clear', function(){
-        graph.clear();
-        socket.broadcast.emit('clear');
+        socket.graph.clear();
+        socket.broadcast.to(socket.room).emit('clear');
     });
 
     socket.on('busy', function(){
-        socket.broadcast.emit('busy');
+        socket.broadcast.to(socket.room).emit('busy');
     });
     
     socket.on('free', function(){
-        socket.broadcast.emit('free');
+        socket.broadcast.to(socket.room).emit('free');
     });
 
     socket.on('load', function(gph){
-        socket.broadcast.emit('load',gph);
+        socket.broadcast.to(socket.room).emit('load',gph);
     });
 
     socket.on('addCell', function(cell){
-        graph.addCells([cell]);
-        socket.broadcast.emit('addCell',cell);
+        socket.graph.addCells([cell]);
+        socket.broadcast.to(socket.room).emit('addCell',cell);
     });
     
     socket.on('removeCell', function(cell){
-        oldCell = graph.getCell(cell.id);
+        oldCell = socket.graph.getCell(cell.id);
             if (oldCell) {oldCell.remove();}
-        socket.broadcast.emit('removeCell',cell.id);
+        socket.broadcast.to(socket.room).emit('removeCell',cell.id);
     });
 
     socket.on('moveCell', function(msg){
-        socket.broadcast.emit('moveCell',msg);
+        socket.broadcast.to(socket.room).emit('moveCell',msg);
     });
     
     socket.on('moveLink', function(msg){
-        socket.broadcast.emit('moveLink',msg);
+        socket.broadcast.to(socket.room).emit('moveLink',msg);
     });
     
     socket.on('upLink', function(msg){
-        mLink = graph.getCell(msg.id);
+        mLink = socket.graph.getCell(msg.id);
     if (msg.vertices !== undefined){
         mLink.set('vertices',msg.vertices);
     }
@@ -82,12 +94,12 @@ io.on('connection', function (socket){
     });
 
         socket.on('upCell', function(msg){
-        mCell = graph.getCell(msg.id);
+        mCell = socket.graph.getCell(msg.id);
         mCell.translate(msg.x-mCell.get('position').x, msg.y-mCell.get('position').y);
     });
     
     socket.on('upGroup', function(msg){
-        mCell = graph.getCell(msg.id);
+        mCell = socket.graph.getCell(msg.id);
         /*
         var inside= msg.inside;
         console.log(inside.length,inside[0].id);
@@ -102,22 +114,22 @@ io.on('connection', function (socket){
         }
         */
         mCell.translate(msg.x-mCell.get('position').x, msg.y-mCell.get('position').y);
-        socket.broadcast.emit('upGroup',msg);
+        socket.broadcast.to(socket.room).emit('upGroup',msg);
     });
     
     socket.on('resizeCell', function(msg){
-        mCell = graph.getCell(msg.id);
+        mCell = socket.graph.getCell(msg.id);
         var inside= mCell.getEmbeddedCells();
         for (i = 0; i < inside.length; i++) {
         mCell.unembed(inside[i]);
     };
     mCell.position(msg.x,msg.y);
     mCell.resize(msg.w,msg.h);
-        socket.broadcast.emit('resizeCell',msg);
+        socket.broadcast.to(socket.room).emit('resizeCell',msg);
     });
     
     socket.on('selectCell', function(msg){
-        socket.broadcast.emit('selectCell',msg);
+        socket.broadcast.to(socket.room).emit('selectCell',msg);
     });
 
     socket.on('savepng', function(msg){
@@ -142,6 +154,18 @@ io.on('connection', function (socket){
           app.get('/'+text, function (req, res) {
             res.sendfile(__dirname + '/index3.html');
               });
+          socket.leave(socket.room);
+          socket.join(text);
+          if (text in graphdict){
+             console.log('graph in room exists already');
+            }
+            else{
+                console.log('new grpah');
+                graphdict[text]=new joint.dia.Graph;
+            }
+          socket.room=text;
+          console.log('new room '+socket.room);
+          socket.graph=graphdict[text];
           socket.emit('moveroom', text);
         }
         else {
